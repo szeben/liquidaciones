4df15b46fcb217fbef857d769e7fc140b5577b88
julieta <thethyhun@hotmail.com>
2023-02-09 17:36:50 +0000

    esqueleto inicial

---
+++ activities_by_delivery_notes/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Actividades por Conduce",
+
+    'summary': """
+        Permite configurar actividades que se generaran automáticamente al crear un movimiento de inventario.
+    """,
+
+    'description': """
+        Permite configurar actividades que se generaran automáticamente al crear un movimiento de inventario.
+    """,
+
+    'author': "Techne Studio IT & Consulting",
+    'website': "https://technestudioit.com/",
+
+    'category': 'Inventory',
+    'version': '0.1',
+
+    'license': 'Other proprietary',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
