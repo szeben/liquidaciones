65bc5b17ac92afb22cc01f8b85621b038857a234
julieta <thethyhun@hotmail.com>
2023-02-13 10:24:33 +0000

    lista: versión 15

--- activities_by_delivery_notes/models/.ipynb_checkpoints/stock_picking_extendido-checkpoint.py
+++ activities_by_delivery_notes/models/.ipynb_checkpoints/stock_picking_extendido-checkpoint.py
@@ -1,46 +1,43 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
+from datetime import datetime
 
 class StockPickingExtendido(models.Model):
     _inherit = "stock.picking"
-    
-    def write(self, vals_list):
-        res = super(StockPickingExtendido, self).write(vals_list)
-        self.escribir_actividad()
+             
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        res.escribir_actividad()    
         return res
-    
+
+
     def escribir_actividad(self):
-        for pick in self:
-            type_id = pick.picking_type_id
-            if type_id and type_id.code == 'outgoing':
-                
-                vence_en_range = type_id.activity_time_range
-                vence_en_date = type_id._return_selection()
-                summary = type_id.activity_summary
-                usuario = pick.create_uid.display_name
-                tipo_act = type_id.activity_type_id.name
-                creacion = pick.create_date
-                creador = pick.create_uid.display_name
-                vence_en = type_id.activity_date
-                
-                display_msg = f"""<b style='color:green;'>Vence en {vence_en_range} {vence_en_date}:</b> 
-                                <b style='font-weight: bolder;'>{summary}</b> para {usuario} <br/><br/>
-                                <b style='font-weight: bolder;'>Tipo de Actividad:</b> {tipo_act} <br/><br/>
-                                <b style='font-weight: bolder;'>Creada:</b> {creacion} {creador} <br/><br/>
-                                <b style='font-weight: bolder;'>Asignada a:</b> {creador} <br/><br/>
-                                <b style='font-weight: bolder;'>Vence el: </b><b style='color:green;'>{vence_en}</b>"""
-                
-                pick.message_post(body=display_msg)
-    
+        type_id = self.picking_type_id
+        if type_id and type_id.code == 'outgoing':
 
-    
+            summary = type_id.activity_summary 
+            tipo_act = type_id.activity_type_id 
+            creacion = self.create_date.date()
+            creador = self.user_id
+            vence_en = type_id.get_due(fields.Date.today())
+            #vence_fmt = datetime.strptime(str(vence_en), '%d/%m/%Y')
 
-                
-                
-                
-                
-                   
-    
-  
+            actividad = self.env['mail.activity'].search([('res_id', '=', self.id), 
+                                                          ('activity_type_id', '=', tipo_act.id)])
+
+
+            self.env['mail.activity'].create({
+                'note': f"""<b style='font-weight: bolder;'>Tipo de Actividad:</b> {tipo_act.display_name} <br/><br/>
+                        <b style='font-weight: bolder;'>Creada:</b> {creacion} por {creador.display_name} <br/><br/>
+                        <b style='font-weight: bolder;'>Asignada a:</b> {creador.display_name} <br/><br/>
+                        <b style='font-weight: bolder;'>Vence el: </b><b style='color:green;'>{vence_en}</b>""",
+                'summary': summary,
+                'date_deadline': vence_en,
+                'user_id': creador.id,
+                'res_model_id': self.env['ir.model'].search([('model', '=', 'stock.picking')]).id,
+                'res_id': self.id,
+                'activity_type_id': tipo_act.id
+            })
 
 
--- activities_by_delivery_notes/models/.ipynb_checkpoints/stock_type_extendido-checkpoint.py
+++ activities_by_delivery_notes/models/.ipynb_checkpoints/stock_type_extendido-checkpoint.py
@@ -9,16 +9,16 @@ class StockTypeExtendido(models.Model):
     
     #ids / info
     mail_act = fields.Many2one(comodel_name="mail.activity", string="Actividad")
-    #activity_res_model = fields.Char(related="mail_act.res_model", store=True, readonly=False)
+    #activity_res_model_id = fields.Char(related="mail_act.res_model_id", store=True, readonly=False)
     activity_type_id = fields.Many2one(related='mail_act.activity_type_id', string='Tipo de Actividad',
                                        store=True, readonly=False,
-                                       domain="['|', ('res_model', '=', False), ('res_model', '=', 'stock.picking.type')]")
+                                       domain="['|', ('res_model', '=', False), ('res_model', '=', 'stock.picking')]")
 
     activity_summary = fields.Char(related="mail_act.summary", string="Resumen", store=True, readonly=False)
     
     
     #dates
-    activity_date = fields.Date(related="mail_act.date_deadline", string="Fecha de Vencimiento", store=True, readonly=False)
+    activity_date = fields.Date(related="mail_act.date_deadline", string="Fecha de Vencimiento", store=True, readonly=False, default=datetime.strptime(str(fields.Date.today()), '%Y-%m-%d'))
     activity_time_range = fields.Integer(default=1, string="Fecha de Vencimiento en", required=True)
     activity_date_range = fields.Selection([
         ('days', 'Día(s)'),
@@ -27,15 +27,12 @@ class StockTypeExtendido(models.Model):
         default="days",
         required=True,
     )
-    
-    def _return_selection(self):
-        return dict(self._fields['activity_date_range'].selection).get(self.activity_date_range)
+
     
     @api.onchange('activity_time_range')
     def _onchange_activity_time_range(self):
         if self.activity_time_range < 0:
             raise ValidationError("La fecha de vencimiento no puede ser negativa.")
-    
     
     #Máximo 5 años
     @api.constrains('activity_time_range', 'activity_date_range')
@@ -50,23 +47,24 @@ class StockTypeExtendido(models.Model):
             elif date == 'years' and time > 5:
                 raise ValidationError('Rango de años inválido (hasta 5 años).') 
                     
-                    
-    @api.onchange('activity_time_range','activity_date_range')
-    def _onchange_range(self):
-        self.activity_date = datetime.strptime(str(fields.Date.today()), '%Y-%m-%d')
+                
+    def get_due(self,time):
+        self.activity_date = datetime.strptime(str(time), '%Y-%m-%d')
         if self.activity_date_range == 'days':
             self.activity_date += relativedelta(days =+ self.activity_time_range)
         elif self.activity_date_range == 'months':
             self.activity_date += relativedelta(months =+ self.activity_time_range)
         elif self.activity_date_range == 'years':
-            self.activity_date += relativedelta(years =+ self.activity_time_range)
+            self.activity_date += relativedelta(years =+ self.activity_time_range) 
+        return self.activity_date
+        
                 
     
-    #user   
+    #user, NO es necesario   
     activity_user_field_name = fields.Char('Usuario', default="user_id", required=True, store=True)
     activity_user_type = fields.Selection([
         ('specific', 'Specific User'),
-        ('generic', 'Generic User From Record')], default="specific", required=True, store=True)
+        ('generic', 'Generic User From Record')], default="generic", required=True, store=True)
     
 
     
