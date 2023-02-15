82de0a0af16be3323ac3fb526e2af8224f118bcc
julieta <thethyhun@hotmail.com>
2023-02-15 23:29:18 +0000

    esqueleto inicial

---
+++ new_expected_inventory_status/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Nuevo Estado de Inventario Pronosticado",
+
+    'summary': """
+        Modifica el icono de inventario pronosticado en la orden de venta para considerar un cumplimiento parcial de un pedido.
+    """,
+
+    'description': """
+        Modifica el icono de inventario pronosticado en la orden de venta para considerar un cumplimiento parcial de un pedido.
+    """,
+
+    'author': "Techne Studio IT & Consulting",
+    'website': "https://technestudioit.com/",
+
+    'category': 'Sales',
+    'version': '0.1',
+
+    'license': 'Other proprietary',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
