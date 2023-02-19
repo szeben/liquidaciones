5daeaafadc229323ff72f8736efdf552c918f362
julieta <thethyhun@hotmail.com>
2023-02-19 06:54:36 +0000

    listo: versión 16

--- new_expected_inventory_status/__manifest__.py
+++ new_expected_inventory_status/__manifest__.py
@@ -19,12 +19,18 @@
     'license': 'Other proprietary',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale'],
+    'depends': ['base', 'sale', 'sale_stock'],
 
     # always loaded
     'data': [
+        'views/sale_stock_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
     ],
+    'assets': {
+        'web.assets_backend': [
+            'new_expected_inventory_status/static/src/widgets/**/*',
+        ],
+    },
 }
