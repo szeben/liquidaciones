9898bce918bbfbafc73e98610966cc055a758204
julieta <thethyhun@hotmail.com>
2023-02-24 06:37:24 +0000

    esqueleto inicial

---
+++ allocation_and_consumption/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Asignación y Consumo de Equipos y Materiales",
+
+    'summary': """
+        Permite registra la asignación y consumo de equipos y materiales empleados de manera interna.
+    """,
+
+    'description': """
+        Incluye movimientos de inventario y campos adicionales, 
+        que permiten el registro de consumo de equipos y materiales, 
+        así como la asignación de los mismos a empleados, departamentos o vehículos.
+    """,
+
+    'author': "Techne Studio IT & Consulting",
+    'website': "https://technestudioit.com/",
+
+    'category': 'Inventory',
+    'version': '0.1',
+
+    'license': 'Other proprietary',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
